142b7bf34762b4c0fa79d78268ca5372c62a2854
MachariaP <walburphinehas78@gmail.com>
2024-06-03 13:51:05 +0300

    Creat, Register and Modified the api, views and urls

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Add our new application to the list of installed apps
+    'api.apps.ApiConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
